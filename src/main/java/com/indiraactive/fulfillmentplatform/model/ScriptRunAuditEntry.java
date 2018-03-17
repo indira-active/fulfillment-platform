2b7f1e6182a476a717eba87807fa42266670e4b5
Miguel Marquez <miguel532013@gmail.com>
2018-03-17 14:25:01 -0500

    Add documentation to ScriptRunAuditEntry

--- src/main/java/com/indiraactive/fulfillmentplatform/model/ScriptRunAuditEntry.java
+++ src/main/java/com/indiraactive/fulfillmentplatform/model/ScriptRunAuditEntry.java
@@ -5,26 +5,48 @@ import java.sql.Timestamp;
 import java.time.DateTimeException;
 import java.util.Date;
 
+/**
+ * Captures a specific run of the sync_inventory script so run history can be inspected.
+ */
 @Entity
 @Table(name = "script_run_audit_entry")
 public class ScriptRunAuditEntry {
+    /**
+     * Id that represents a unique script audit entry
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "script_run_audit_entry_id", length = 64, updatable = false)
     private Integer scriptRunAuditEntryId;
 
+    /**
+     * The supplier ID that was used to populate the script arguments
+     */
     @Column(name = "supplier_id", length = 64, updatable = false)
     private Integer supplierId;
 
+    /**
+     * Whether or not the script run was successful
+     */
     @Column(name = "success_code", nullable = false)
     private String successCode;
 
+    /**
+     * The time the script started running at
+     */
     @Column(name = "start_date_time", nullable = false)
     private long startDateTime;
 
+    /**
+     * The time the script finished running at
+     */
     @Column(name = "finish_date_time", nullable = false)
     private long finishDateTime;
 
+    /**
+     * User who triggered the run. Eventually may need to figure out a way to capture automated runs
+     * that are on a timer.
+     */
     @Column(name = "user_triggered", nullable = false)
     private String userTriggered;
 
